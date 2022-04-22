1f58b7a163f5463dfa3cf21558c43814dd565edb
Josh Justice <me@codingitwrong.com>
2022-04-22 07:50:08 -0400

    Add analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,10 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
+        gtag: {
+          trackingID: 'G-MQY51C1VKW',
+          anonymizeIP: true,
+        },
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
